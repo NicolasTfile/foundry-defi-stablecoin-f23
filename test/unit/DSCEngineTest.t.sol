40d5746f80169999f130c592b531a3eb72176535
NicolasTfile <nicolastfile@gmail.com>
2026-02-12 16:22:45 +0100

    wrote test for collateral reverts

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -7,6 +7,7 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DeployDSC deployer;
@@ -15,6 +16,9 @@ contract DSCEngineTest is Test {
     HelperConfig config;
     address ethUsdPriceFeed;
     address weth;
+
+    address public user = makeAddr("user0");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
 
     function setUp() public {
         deployer = new DeployDSC();
@@ -25,10 +29,24 @@ contract DSCEngineTest is Test {
     ///////////////////
     // Price Tests   //
     ///////////////////
-    function testGetUsdValue() public {
+
+    function testGetUsdValue() public view {
         uint256 ethAmount = 15e18; // 15e18 * 2000/ETH = 30,000e18
         uint256 expectedUsd = 30000e18;
         uint256 actualUsd = dsce.getUsdValue(weth, ethAmount);
         assertEq(expectedUsd, actualUsd);
+    }
+
+    /////////////////////////////
+    // depositCollateral Tests //
+    /////////////////////////////
+
+    function testRevertsIfCollateralZero() public {
+        vm.startPrank(user);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        dsce.depositCollateral(weth, 0);
+        vm.stopPrank();
     }
 }
