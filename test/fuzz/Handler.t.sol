9d35a6475eb17d045aa5a98388d5dc8157fd523b
NicolasTfile <nicolastfile@gmail.com>
2026-02-19 20:27:16 +0100

    wrote invariant tests for dscengine deposit and redeem collateral

---
+++ test/fuzz/Handler.t.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+
+contract Handler is Test {
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+
+    ERC20Mock weth;
+    ERC20Mock wbtc;
+
+    uint256 MAX_DEPOSIT_SIZE = type(uint96).max; // If it is uint256, next deposit (uint256 value + 1) will revert
+
+    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc) {
+        dsce = _dscEngine;
+        dsc = _dsc;
+
+        address[] memory collateralTokens = dsce.getCollateralTokens();
+        weth = ERC20Mock(collateralTokens[0]);
+        wbtc = ERC20Mock(collateralTokens[1]);
+    }
+
+    function depositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_SIZE);
+
+        vm.startPrank(msg.sender);
+        collateral.mint(msg.sender, amountCollateral);
+        collateral.approve(address(dsce), amountCollateral);
+        dsce.depositCollateral(address(collateral), amountCollateral);
+        vm.stopPrank();
+    }
+
+    function redeemCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        uint256 maxCollateraltoRedeem = dsce.getCollateralBalanceOfUser(msg.sender, address(collateral));
+        amountCollateral = bound(amountCollateral, 1, maxCollateraltoRedeem);
+        dsce.redeemCollateral(address(collateral), amountCollateral);
+    }
+
+    // Helper Functions
+    function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock) {
+        if (collateralSeed % 2 == 0) {
+            return weth;
+        }
+        return wbtc;
+    }
+}
